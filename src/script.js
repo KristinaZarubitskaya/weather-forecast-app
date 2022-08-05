62a589fc89e957829c2ab31360c3a3fd4fb65380
KristinaZarubitskaya <107760757+KristinaZarubitskaya@users.noreply.github.com>
2022-08-05 13:30:41 +0300

    made API work

--- src/script.js
+++ src/script.js
@@ -17,6 +17,50 @@ let day = days[now.getDay()];
 
 document.querySelector("#dayTime").innerHTML = `${day} ${hour}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `         <div class="col-2">
+                <div class="forecast-days">${formatDay(forecastDay.dt)}</div>
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
+          alt="" 
+          width="30" />
+                <br />
+                <span id="max-temp-day"> ${Math.round(
+                  forecastDay.temp.max
+                )}°</span>/
+                <span id="min-temp-day">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
+              </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "c48d919f779c3900f03f3a5b1a2af8c1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   document.querySelector("#city-name").innerHTML = response.data.name;
   CTemperature = response.data.main.temp;
@@ -35,6 +79,7 @@ function displayTemperature(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -67,26 +112,6 @@ function displayCTemperature(event) {
   temperatureElement.innerHTML = Math.round(CTemperature);
 }
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `         <div class="col-2">
-                <div class="forecast-days">${day}</div>
-                <img src="img/sun.png" width="30" />
-                <br />
-                <span id="max-temp-day"> 18</span> /
-                <span id="min-temp-day">12</span>
-              </div>`;
-  });
-  forecastHTML = forecastHTML + `</div>`;
-
-  forecastElement.innerHTML = forecastHTML;
-}
-
 let CTemperature = null;
 
 let FLink = document.querySelector("#FLink");
@@ -103,4 +128,3 @@ axios(apiUrl).then(displayTemperature);
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handlesubmit);
 search("Odessa");
-displayForecast();
